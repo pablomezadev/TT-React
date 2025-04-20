27f9eb04a698d4f9bbce00f9ac55ea8158c7f4c2
pablo meza dev <pablomeza.dev@gmail.com>
2025-04-20 04:24:36 -0300

    Primera clase react - jsx

---
+++ src/components/Saludo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Saludo2(params){
+    return(
+        <h2>Soy un componente exportado desde un archivo jsx</h2>
+    )
+}
+
+export default Saludo2 // exportamos por default porque esta unica funcion es lo unico que exportamos
